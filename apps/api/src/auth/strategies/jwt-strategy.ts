2b90f27920b5ad29a7ffd26211a61f9b91d0f473
willy0483 <willyjensen251@gmail.com>
2025-04-26 21:47:06 +0200

    Protecting GraphQL Endpoints #11

---
+++ apps/api/src/auth/strategies/jwt-strategy.ts
@@ -0,0 +1,30 @@
+import { AuthService } from './../auth.service';
+import { ConfigService } from '@nestjs/config';
+import { PassportStrategy } from '@nestjs/passport';
+import { ExtractJwt, Strategy } from 'passport-jwt';
+import { AuthJwtPayload } from '../types/auth-jwtPayload';
+import { Injectable } from '@nestjs/common';
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy) {
+  constructor(
+    private configService: ConfigService,
+    private authService: AuthService,
+  ) {
+    const jwtSecret = configService.get<string>('JWT_SECRET');
+    if (!jwtSecret) {
+      throw new Error('JWT_SECRET is not defined in the configuration');
+    }
+
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      secretOrKey: jwtSecret,
+      ignoreExpiration: false,
+    });
+  }
+
+  validate(payload: AuthJwtPayload) {
+    const userId = payload.sub;
+    return this.authService.validateJwtUser(userId);
+  }
+}
